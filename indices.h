c3d5ec89912f12974ba176aba3d2adfaaa4cf611
German Ferrero <ferrero.gf@gmail.com>
2021-06-02 12:58:38 -0300

    recover original indices.h

--- indices.h
+++ indices.h
@@ -2,10 +2,14 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-function"
 
-static inline size_t rb_idx(size_t y, size_t x, size_t dim) {
+static inline size_t rb_idx(size_t x, size_t y, size_t dim) {
     size_t base = ((x % 2) ^ (y % 2)) * dim * (dim / 2);
     size_t offset = (x / 2) + y * (dim / 2);
     return base + offset;
+}
+
+static size_t idx(size_t x, size_t y, size_t stride) {
+    return x + y * stride;
 }
 
 #pragma GCC diagnostic pop
