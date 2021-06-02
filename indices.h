a62589b47a3845272a3e54c601ac0de36703646e
German Ferrero <ferrero.gf@gmail.com>
2021-06-02 12:56:52 -0300

    add missing indices.h file

---
+++ indices.h
@@ -0,0 +1,11 @@
+#pragma once
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-function"
+
+static inline size_t rb_idx(size_t y, size_t x, size_t dim) {
+    size_t base = ((x % 2) ^ (y % 2)) * dim * (dim / 2);
+    size_t offset = (x / 2) + y * (dim / 2);
+    return base + offset;
+}
+
+#pragma GCC diagnostic pop
